c80094513659422e764823fb197f97c0d6b96387
Babak Mursalov <88366383+BabekMursalov@users.noreply.github.com>
2024-11-23 06:25:31 +0400

    Update settings.py

--- medical_project/settings.py
+++ medical_project/settings.py
@@ -74,27 +74,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'medical_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-			'default': {
-		        'ENGINE': 'mssql',
-		        'HOST': 'BABEK\SQLEXPRESS',
-		        'NAME': 'medical_project',
-		        'USER': 'sa',
-		        'PASSWORD': 'Komutan4506!',
-		        'OPTIONS': {
-		            'driver': 'ODBC Driver 17 for SQL Server',
-		        },
-		    },
-		}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES = {
+# 			'default': {
+# 		        'ENGINE': 'mssql',
+# 		        'HOST': 'BABEK\SQLEXPRESS',
+# 		        'NAME': 'medical_project',
+# 		        'USER': 'sa',
+# 		        'PASSWORD': 'Komutan4506!',
+# 		        'OPTIONS': {
+# 		            'driver': 'ODBC Driver 17 for SQL Server',
+# 		        },
+# 		    },
+# 		}
 
 
 # Password validation
